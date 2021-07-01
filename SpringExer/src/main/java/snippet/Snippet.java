ce8d9fb22f28aa295fa9123d61eceec2623891ab
JEmilyKoo <jemilykoo@gmail.com>
2021-07-01 18:32:32 +0900

    ss

---
+++ SpringExer/src/main/java/snippet/Snippet.java
@@ -0,0 +1,23 @@
+package snippet;
+
+public class Snippet {
+	<div data-mesh-id="comp-kpzke0re1inlineContent-gridContainer"
+				data-testid="mesh-container-content" style="">
+	
+				<div id="comp-kpzke0rg" aria-disabled="false" class="oneButton">
+					<a data-testid="linkElement"
+						href="https://jemilykoo.wixsite.com/website/profile/%7BuserName%7D/profile"
+						target="_self" class="oneButtonLinkClass"><span
+						class="oneButtonSpanClass">게시글</span></a>
+				</div>
+				<div id="comp-kpzkekds" aria-disabled="false" class="oneButton">
+					<a data-testid="linkElement"
+						data-anchor-comp-id="adi_page1017_1_154"
+						href="https://jemilykoo.wixsite.com/website/%ED%94%84%EB%A1%9C%ED%95%84"
+						target="_self" class="oneButtonLinkClass"><span
+						class="oneButtonSpanClass">뱃지</span></a>
+				</div>
+			</div>
+		</div>
+}
+
