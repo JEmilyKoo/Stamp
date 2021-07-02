bb02da2e3f4525c522ab8ae93e3bdab532b60956
kosmo_16 <kosmo_16@DESKTOP-J73795B>
2021-07-02 17:40:48 +0900

    로그인 수정

--- SpringExer/src/main/java/com/company/exer/service/KakaoProfile.java
+++ SpringExer/src/main/java/com/company/exer/service/KakaoProfile.java
@@ -8,7 +8,6 @@ public class KakaoProfile {
 public Integer id;
 public String connected_at;
 public Properties properties;
-public KakaoAccount kakao_account;
 
 	@Data
 	public class Properties {
@@ -17,25 +16,4 @@ public KakaoAccount kakao_account;
 		public String thumbnail_image;
 		}
 	
-	@Data
-	public class KakaoAccount {
-		public Boolean profile_needs_agreement;
-		public Profile profile;
-		public Boolean has_age_range;
-		public Boolean age_range_needs_agreement;
-		public String age_range;
-		public Boolean has_gender;
-		public Boolean gender_needs_agreement;
-		public String gender;
-		
-			@Data
-			public class Profile {
-				public String nickname;
-				public String thumbnail_image_url;
-				public String profile_image_url;
-				public Boolean is_default_image;
-				}
-
-		}
-
 }
