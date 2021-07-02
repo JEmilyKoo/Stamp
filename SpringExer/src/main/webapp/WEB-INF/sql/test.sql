cd42d72dc29d9d7cc826e929b4d512b74c9bf1cc
kosmo_16 <kosmo_16@DESKTOP-J73795B>
2021-07-02 16:22:39 +0900

    테이블 정리

--- SpringExer/src/main/webapp/WEB-INF/sql/test.sql
+++ SpringExer/src/main/webapp/WEB-INF/sql/test.sql
@@ -1,63 +1,8 @@
-CREATE TABLE MEMBER
-(
-	id varchar2(50) PRIMARY KEY,
-	pwd varchar2(10) NOT NULL,
-	name nvarchar2(10) NOT NULL
-);
-
-CREATE TABLE REVIEW
-(
-	rvNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
-	rvTitle nvarchar2(20) NOT NULL,
-	rvCtt clob,
-	rvLikeCnt number DEFAULT 0,
-	rvLat number,
-	rvLng number
-);
 
 
-CREATE TABLE STAMP
-(
-	stNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
-	rvNo number references REVIEW(rvNo) on delete cascade,
-	stDate date DEFAULT SYSDATE
-);
-
-CREATE TABLE RVLIKE
-(
-	id varchar2(50) references MEMBER(id) on delete cascade,
-	rvNo number references REVIEW(rvNo) on delete cascade
-);
-
-CREATE TABLE FOLLOW
-(
-	followNo number PRIMARY KEY,
-	follwerid varchar2(50) references MEMBER(id) on delete cascade,
-	followId varchar2(50) references MEMBER(id) on delete cascade
-);
-
-
-CREATE SEQUENCE SEQ_REVIEW
-nocache
-nocycle;
-
-CREATE SEQUENCE SEQ_FOLLOW
-nocache
-nocycle;
-
-CREATE SEQUENCE SEQ_RVLIKE
-nocache
-nocycle;
-
-CREATE SEQUENCE SEQ_STAMP
-nocache
-nocycle;
 
 
 /* Drop Tables 
-
 DROP TABLE ACHLIST CASCADE CONSTRAINTS;
 DROP TABLE BADGELIST CASCADE CONSTRAINTS;
 DROP TABLE DM CASCADE CONSTRAINTS;
@@ -66,39 +11,38 @@ DROP TABLE FBCMNT CASCADE CONSTRAINTS;
 DROP TABLE FBLIKE CASCADE CONSTRAINTS;
 DROP TABLE FOLLOW CASCADE CONSTRAINTS;
 DROP TABLE FREEBOARD CASCADE CONSTRAINTS;
-DROP TABLE PROFILE CASCADE CONSTRAINTS;
 DROP TABLE RVCMNT CASCADE CONSTRAINTS;
 DROP TABLE RVLIKE CASCADE CONSTRAINTS;
 DROP TABLE STAMP CASCADE CONSTRAINTS;
 DROP TABLE REVIEW CASCADE CONSTRAINTS;
+DROP TABLE PROFILE CASCADE CONSTRAINTS;
 DROP TABLE MEMBER CASCADE CONSTRAINTS;
-
 */
 
 
-/* Create Tables
+/* Create Tables */
 
 CREATE TABLE ACHLIST
 (
-	achId nvarchar2(20) PRIMARY KEY ,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	achId nvarchar2(20) primary key,
+	nickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
 	achDate date DEFAULT SYSDATE
 );
 
 
 CREATE TABLE BADGELIST
 (
-	bgId nvarchar2(20) PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	bgId nvarchar2(20) primary key,
+	nickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
 	bgDate date DEFAULT SYSDATE
 );
 
 
 CREATE TABLE DM
 (
-	dmNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
-	dmToId varchar2(50) references MEMBER(id) on delete cascade,
+	dmNo number primary key,
+	nickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
+	dmToNickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
 	dmCnt nvarchar2(100),
 	dmDate date DEFAULT SYSDATE,
 	dmChecked number DEFAULT 1
@@ -107,8 +51,7 @@ CREATE TABLE DM
 
 CREATE TABLE FAVORITE
 (
-	favoriteNo number PRIMARY KEY ,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	favoriteNo number primary key,
 	rvNo number references REVIEW(rvNo) on delete cascade,
 	FavoriteRegiDate date DEFAULT SYSDATE
 );
@@ -116,8 +59,7 @@ CREATE TABLE FAVORITE
 
 CREATE TABLE FBCMNT
 (
-	fbcNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	fbcNo number primary key,
 	fbNo number references FREEBOARD(fbNo) on delete cascade,
 	fbCmnt clob,
 	fbcDate date DEFAULT SYSDATE
@@ -126,8 +68,7 @@ CREATE TABLE FBCMNT
 
 CREATE TABLE FBLIKE
 (
-	fblNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	fblNo number primary key,
 	fbNo number references FREEBOARD(fbNo) on delete cascade,
 	fblDate date DEFAULT SYSDATE
 );
@@ -135,16 +76,16 @@ CREATE TABLE FBLIKE
 
 CREATE TABLE FOLLOW
 (
-	followNo number PRIMARY KEY,
-	follwerid varchar2(50) references MEMBER(id) on delete cascade,
-	followId varchar2(50) references MEMBER(id) on delete cascade
+	followNo number primary key,
+	followerNickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
+	followIdNickname nvarchar2(15) references PROFILE(nickname) on delete cascade
 );
 
 
 CREATE TABLE FREEBOARD
 (
-	fbNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	fbNo number primary key,
+	nickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
 	fbTitle nvarchar2(20),
 	fbCtt clob,
 	fbDate date DEFAULT SYSDATE,
@@ -156,34 +97,34 @@ CREATE TABLE FREEBOARD
 
 CREATE TABLE MEMBER
 (
-	id varchar2(50) PRIMARY KEY,
+	id varchar2(50) primary key,
 	pwd varchar2(10) NOT NULL,
 	name nvarchar2(10) NOT NULL,
 	regiDate date DEFAULT SYSDATE,
-	gender nvarchar2(8),
-	mail nvarchar2(100),
-	phone nvarchar2(15),
-	birth nvarchar2(15)
+	mail nvarchar2(100)
 );
 
 
 CREATE TABLE PROFILE
 (
+	nickname nvarchar2(15) primary key,
 	id varchar2(50) references MEMBER(id) on delete cascade,
-	nickname nvarchar2(15) UNIQUE,
 	trvPrpns nvarchar2(20),
 	exp number DEFAULT 0,
 	level number DEFAULT 0,
 	pr nvarchar2(100),
 	openPrf number DEFAULT 1,
-	prfImage clob
+	prfImage clob,
+	gender nvarchar2(8),
+	birth nvarchar2(15),
+	phone nvarchar2(15)
 );
 
 
 CREATE TABLE REVIEW
 (
-	rvNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	rvNo number primary key,
+	nickname nvarchar2(15) references PROFILE(nickname) on delete cascade,
 	rvTitle nvarchar2(20) NOT NULL,
 	rvCtt clob,
 	rvLikeCnt number DEFAULT 0,
@@ -200,8 +141,7 @@ CREATE TABLE REVIEW
 
 CREATE TABLE RVCMNT
 (
-	rvcNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	rvcNo number primary key,
 	rvNo number references REVIEW(rvNo) on delete cascade,
 	rvCmnt clob,
 	rvcDate date DEFAULT SYSDATE
@@ -210,149 +150,19 @@ CREATE TABLE RVCMNT
 
 CREATE TABLE RVLIKE
 (
-	rvlNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
-	rvNo number references REVIEW(rvNo) on delete cascade,
+	rvlNo number primary key,
+	rvNo references REVIEW(rvNo) on delete cascade,
 	rvDate date DEFAULT SYSDATE
 );
 
 
 CREATE TABLE STAMP
 (
-	stNo number PRIMARY KEY,
-	id varchar2(50) references MEMBER(id) on delete cascade,
+	stNo number primary key,
 	rvNo number references REVIEW(rvNo) on delete cascade,
 	stDate date DEFAULT SYSDATE,
 	stIsExpired number DEFAULT 1
 );
-
-
-
-
-ALTER TABLE FBCMNT
-	ADD FOREIGN KEY (fbNo)
-	REFERENCES FREEBOARD (fbNo)
-;
-
-
-ALTER TABLE FBLIKE
-	ADD FOREIGN KEY (fbNo)
-	REFERENCES FREEBOARD (fbNo)
-;
-
-
-ALTER TABLE ACHLIST
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE BADGELIST
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE DM
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE DM
-	ADD FOREIGN KEY (dmToId)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FAVORITE
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FBCMNT
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FBLIKE
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FOLLOW
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FOLLOW
-	ADD FOREIGN KEY (followId)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FREEBOARD
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE PROFILE
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE REVIEW
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE RVCMNT
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE RVLIKE
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE STAMP
-	ADD FOREIGN KEY (id)
-	REFERENCES MEMBER (id)
-;
-
-
-ALTER TABLE FAVORITE
-	ADD FOREIGN KEY (rvNo)
-	REFERENCES REVIEW (rvNo)
-;
-
-
-ALTER TABLE RVCMNT
-	ADD FOREIGN KEY (rvNo)
-	REFERENCES REVIEW (rvNo)
-;
-
-
-ALTER TABLE RVLIKE
-	ADD FOREIGN KEY (rvNo)
-	REFERENCES REVIEW (rvNo)
-;
-
-
-ALTER TABLE STAMP
-	ADD FOREIGN KEY (rvNo)
-	REFERENCES REVIEW (rvNo)
-;
 
 CREATE SEQUENCE SEQ_REVIEW
 nocache
@@ -397,4 +207,97 @@ nocycle;
 CREATE SEQUENCE SEQ_STAMP
 nocache
 nocycle;
- */
+
+
+
+
+/* Create Foreign Keys 
+ALTER TABLE FBCMNT
+	ADD FOREIGN KEY (fbNo)
+	REFERENCES FREEBOARD (fbNo)
+;
+
+
+ALTER TABLE FBLIKE
+	ADD FOREIGN KEY (fbNo)
+	REFERENCES FREEBOARD (fbNo)
+;
+
+
+ALTER TABLE PROFILE
+	ADD FOREIGN KEY (id)
+	REFERENCES MEMBER (id)
+;
+
+
+ALTER TABLE ACHLIST
+	ADD FOREIGN KEY (nickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE BADGELIST
+	ADD FOREIGN KEY (nickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE DM
+	ADD FOREIGN KEY (nickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE DM
+	ADD FOREIGN KEY (dmToNickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE FOLLOW
+	ADD FOREIGN KEY (followerNicname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE FOLLOW
+	ADD FOREIGN KEY (followIdNickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE FREEBOARD
+	ADD FOREIGN KEY (nickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE REVIEW
+	ADD FOREIGN KEY (nickname)
+	REFERENCES PROFILE (nickname)
+;
+
+
+ALTER TABLE FAVORITE
+	ADD FOREIGN KEY (rvNo)
+	REFERENCES REVIEW (rvNo)
+;
+
+
+ALTER TABLE RVCMNT
+	ADD FOREIGN KEY (rvNo)
+	REFERENCES REVIEW (rvNo)
+;
+
+
+ALTER TABLE RVLIKE
+	ADD FOREIGN KEY (rvNo)
+	REFERENCES REVIEW (rvNo)
+;
+
+
+ALTER TABLE STAMP
+	ADD FOREIGN KEY (rvNo)
+	REFERENCES REVIEW (rvNo)
+;
+*/
