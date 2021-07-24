13f58da26c00681d98590d27517be76808f9bb43
JEmilyKoo <jemilykoo@gmail.com>
2021-07-25 00:05:00 +0900

    쿼리 맞춰서 rvlike FBCmnt 수정

--- SpringExer/src/main/java/com/company/exer/service/FBCmntDTO.java
+++ SpringExer/src/main/java/com/company/exer/service/FBCmntDTO.java
@@ -8,7 +8,7 @@ public class FBCmntDTO {
 	private int fbNo;
 	private String fbCmnt;
 	private Timestamp fbcDate;
-	//private String nickName;(필요한데 SQL에 없음)
+	private String nickName;
 	//FBCMNT(자유게시판댓글 Free board comments))
 
 }
